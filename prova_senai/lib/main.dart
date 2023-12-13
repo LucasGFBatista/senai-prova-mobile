bdad489bfa15ef19668bf9350e30664ba8e2a069
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-12-13 14:24:24 -0300

    feat: Cria tela de login

--- prova_senai/lib/main.dart
+++ prova_senai/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:prova_senai/pages/login.page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+@override
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HiperLínguas',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ prova_senai/lib/pages/login.page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[50],
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Colors.black, fontSize: 30),
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+              Divider(),
+              TextFormField(
+                autofocus: true,
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(color: Colors.black, fontSize: 30),
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(color: Colors.black),
+                ),
+              ),
+              Divider(),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  // Adicione a lógica para o botão "Entrar" aqui
+                },
+                child: Text(
+                  "Entrar",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
