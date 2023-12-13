bf0e75c2137ab78e68b1473b3a9c1a4ef7e4de4b
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-12-13 16:02:51 -0300

    feat: Create singup page and onboard page

---
+++ prova_senai/lib/pages/singup.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingupPage extends StatefulWidget {
+  const SingupPage({super.key});
+
+  @override
+  State<SingupPage> createState() => _SingupPageState();
+}
+
+class _SingupPageState extends State<SingupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
