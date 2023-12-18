f783f520c40658820828392cd15b991e1a72fb3d
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-12-18 13:55:16 -0300

    feat: Autenticação de usuario

--- prova_senai/lib/services/auth_service.dart
+++ prova_senai/lib/services/auth_service.dart
@@ -1,21 +1,18 @@
 import 'package:prova_senai/services/databaseLinguas.dart';
 
 class AuthService {
-  final DatabaseLinguas _databaseLinguas = DatabaseLinguas();
+
 
   Future<bool> login(String email, String password) async {
-    // Aqui devemos implementar a lógica de autenticação.
-    // Pode ser uma chamada a uma API, validação local, etc.
+    
+    Map<String, dynamic>? user = await DatabaseLinguas.getUserByEmail(email);
 
-    // Por exemplo, vamos verificar no banco de dados local.
-    List<Map<String, dynamic>> users = await _databaseLinguas.getUsers();
-
-    for (var user in users) {
-      if (user['email'] == email && user['password'] == password) {
-        return true;
-      }
+    if (user != null && user['password'] == password) {
+      // Usuário autenticado com sucesso
+      return true;
+    } else {
+      // Senha incorreta ou usuário não encontrado
+      return false;
     }
-
-    return false;
   }
 }
