5b52393eee8383074f0e08db08b3d119559e2a0f
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-12-18 14:47:35 -0300

    feat: Metodo de verificação de email já cadastrado

--- prova_senai/lib/services/auth_service.dart
+++ prova_senai/lib/services/auth_service.dart
@@ -1,10 +1,7 @@
 import 'package:prova_senai/services/databaseLinguas.dart';
 
 class AuthService {
-
-
   Future<bool> login(String email, String password) async {
-    
     Map<String, dynamic>? user = await DatabaseLinguas.getUserByEmail(email);
 
     if (user != null && user['password'] == password) {
@@ -13,6 +10,19 @@ class AuthService {
     } else {
       // Senha incorreta ou usuário não encontrado
       return false;
+    }
+  }
+
+  // Metodo para verificação se email já está cadastrado
+  Future<bool> verifyNewAccount(String email) async {
+    Map<String, dynamic>? user = await DatabaseLinguas.getUserByEmail(email);
+
+    if (user != null && user['email'] == email) {
+      //email já cadastrado,
+      return false;
+    } else {
+      //email não cadastrado, pode cadastrar nova conta
+      return true;
     }
   }
 }
