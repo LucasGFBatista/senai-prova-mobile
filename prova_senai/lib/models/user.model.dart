f8184a94fd457704efa25586a817c9198308c98d
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-12-21 11:30:06 -0300

    feat: Tentando refatorar Criado model user

---
+++ prova_senai/lib/models/user.model.dart
@@ -0,0 +1,11 @@
+class User {
+  final String userName;
+  final String email;
+  final dynamic password;
+
+  User({
+    required this.userName,
+    required this.email,
+    required this.password,
+  });
+}
