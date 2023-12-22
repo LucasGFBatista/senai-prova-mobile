43fe559f3cfef96d2cc6f4a26992c071811b8b4e
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-12-22 12:32:46 -0300

    feat: Cria progress model

---
+++ prova_senai/lib/models/progress.model.dart
@@ -0,0 +1,11 @@
+class Progress {
+  final double percentage;
+  final int correctAnswers;
+  final int totalQuestions;
+
+  Progress({
+    required this.percentage,
+    required this.correctAnswers,
+    required this.totalQuestions,
+  });
+}
